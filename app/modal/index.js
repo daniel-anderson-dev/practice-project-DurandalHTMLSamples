ae706ae4a85977d52eedcc38d422e303fd0ff91c
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-05-06 14:35:07 -0400

    Setup the custom Modal display.

---
+++ app/modal/index.js
@@ -0,0 +1,9 @@
+define(['durandal/app', './customModal'], function(app, CustomModal) {
+	return {
+		showCustomModal: function() {
+			CustomModal.show().then(function(response) {
+				app.showMessage('You specified: ' + response + '.');
+			});
+		}
+	};
+});
