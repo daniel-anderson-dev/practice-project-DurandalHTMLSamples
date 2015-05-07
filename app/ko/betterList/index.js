037b6386401d9b64e3f002098152f3af819b7e16
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-05-07 16:08:36 -0400

    Setup the KO Better List section.

---
+++ app/ko/betterList/index.js
@@ -0,0 +1,31 @@
+﻿define(['knockout'], function(ko) {
+
+    var placeholder = "...";
+
+    var itemToAdd = ko.observable(placeholder);
+    var allItems = ko.observableArray(["Fries", "Eggs Benedict", "Hamz", "Cheese"]);
+    var selectedItems = ko.observableArray(["Hamz", "Fries"]);
+
+    return {
+        itemToAdd: itemToAdd,
+        allItems: allItems,
+        selectedItems: selectedItems,
+        addItem: function() {
+            var value = itemToAdd();
+
+            if (value != "" && allItems.indexOf(value) < 0) {
+                allItems.push(value);
+            }
+
+            itemToAdd(placeholder);
+        },
+        removeSelected: function() {
+            allItems.removeAll(selectedItems());
+            selectedItems([]);
+        },
+        sortItems: function() {
+            allItems.sort();
+        }
+    }
+
+});
