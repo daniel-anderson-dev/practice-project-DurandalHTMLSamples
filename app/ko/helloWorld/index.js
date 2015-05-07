b3b73e3ffb358a52c9d2f170bc15c17a78214eca
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-05-07 15:12:43 -0400

    Added the KO Hello World sample.

---
+++ app/ko/helloWorld/index.js
@@ -0,0 +1,16 @@
+﻿define(['knockout'], function(ko) {
+
+    var firstName = ko.observable("Planet");
+    var lastName = ko.observable("Earth");
+
+    var fullName = ko.computed(function () {
+        return firstName() + " " + lastName();
+    });
+
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        fullName: fullName
+    }
+
+});
