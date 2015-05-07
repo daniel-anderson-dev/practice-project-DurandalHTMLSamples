e3d15a501154eb6c3f3906316c85d317e9da26b3
daniel-anderson <daniel.anderson28562@hotmail.com>
2015-05-07 16:37:46 -0400

    Setup the KO Control Types section.

---
+++ app/ko/controlTypes/index.js
@@ -0,0 +1,11 @@
+﻿define(['knockout'], function(ko) {
+	return {
+		stringValue: ko.observable("Hello"),
+		passwordValue: ko.observable("MyPassword"),
+		booleanValue: ko.observable(true),
+		optionValues: ['Alpha', 'Beta', 'Gamma'],
+		selectedOptionValue: ko.observable("Gamma"),
+		multipleSelectedOptionValues: ko.observable(["Alpha"]),
+		radioSelectedOptionValue: ko.observable("Beta")
+	}
+});
